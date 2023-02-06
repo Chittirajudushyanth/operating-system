213cd97aafb7a3057fb119603dc299a9c412ee4b
chitraju dushyanth <113229130+Chittirajudushyanth@users.noreply.github.com>
2023-02-06 08:48:48 +0530

    Create 30.3 program.c

---
+++ 30.3 program.c
@@ -0,0 +1,13 @@
+
+#include <pthread.h> 
+ #include <stdio.h> 
+
+ int main () 
+ { 
+    pthread_t thread1, thread2; 
+
+    printf("Before thread creation, thread1 = %ld, thread2 = %ld\n", thread1, thread2); 
+
+    pthread_create; 
+    return 0; 
+    }
